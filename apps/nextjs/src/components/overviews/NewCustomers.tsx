a3d514b29c78ffe72c49ab2db9ae791dadb2ac56
rNNdom <l.galeano01@ufromail.cl>
2023-12-03 06:30:16 -0300

    fixed styles

--- apps/nextjs/src/components/overviews/NewCustomers.tsx
+++ apps/nextjs/src/components/overviews/NewCustomers.tsx
@@ -23,31 +23,34 @@ function NewCustomers () {
 
   return (
     <Card>
-      {fetchedData.isLoading && <Loading />}
-      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-        <CardTitle className="text-sm font-medium">Nuevos clientes</CardTitle>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          className="text-muted-foreground h-4 w-4"
-        >
-          <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-          <circle cx="9" cy="7" r="4" />
-          <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
-        </svg>
-      </CardHeader>
-      <CardContent>
-        <div className="text-2xl font-bold">+{lastItem}</div>
-        <p className="text-muted-foreground text-xs">
-          {percentage > 0 ? `+${percentage.toFixed(2)}%` : `-${percentage.toFixed(2)}%`} desde el mes anterior{" "}
-        </p>
-      </CardContent>
-      <NewCustomersChart data={data} />
+      {fetchedData.isLoading ? <Loading /> :
+        <>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">Nuevos clientes</CardTitle>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="text-muted-foreground h-4 w-4"
+            >
+              <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+              <circle cx="9" cy="7" r="4" />
+              <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
+            </svg>
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">+{lastItem}</div>
+            <p className="text-muted-foreground text-xs">
+              {percentage > 0 ? `+${percentage.toFixed(2)}%` : `-${percentage.toFixed(2)}%`} desde el mes anterior{" "}
+            </p>
+          </CardContent>
+          <NewCustomersChart data={data} />
+        </>
+      }
     </Card>
   )
 }
--- apps/nextjs/src/components/overviews/TotalSales.tsx
+++ apps/nextjs/src/components/overviews/TotalSales.tsx
@@ -34,31 +34,34 @@ function TotalSales () {
 
   return (
     <Card>
-      {fetchedData.isLoading && <Loading />}
-      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-        <CardTitle className="text-sm font-medium">
-          Ingresos totales
-        </CardTitle>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          className="text-muted-foreground h-4 w-4"
-        >
-          <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-        </svg>
-      </CardHeader>
-      <CardContent>
-        <div className="text-2xl font-bold">{formatter.format(lastItem)}</div>
-        <p className="text-muted-foreground text-xs">
-          {percentage > 0 ? `+${percentage.toFixed(2)}%` : `-${percentage.toFixed(2)}%`} desde el mes anterior{" "}
-        </p>
-      </CardContent>
-      <TotalSalesChart data={chartData} />
+      {fetchedData.isLoading ? <Loading /> :
+        <>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">
+              Ingresos totales
+            </CardTitle>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="text-muted-foreground h-4 w-4"
+            >
+              <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
+            </svg>
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">{formatter.format(lastItem)}</div>
+            <p className="text-muted-foreground text-xs">
+              {percentage > 0 ? `+${percentage.toFixed(2)}%` : `-${percentage.toFixed(2)}%`} desde el mes anterior{" "}
+            </p>
+          </CardContent>
+          <TotalSalesChart data={chartData} />
+        </>
+      }
     </Card>
   )
 }
